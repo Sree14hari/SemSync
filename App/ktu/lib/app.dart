33d16aec335213e67b190afbb729af14c4ae8575
SREEHARI <sreehari14shr@gmail.com>
2025-07-12 19:56:12 +0530

    added color swatches

--- App/ktu/lib/app.dart
+++ App/ktu/lib/app.dart
@@ -1,13 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class KtuApp extends StatelessWidget {
   const KtuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return WidgetsApp(
-      color: CupertinoColors.systemGrey,
-      home: const Center(child: Text('Hello, KTU!')),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "SEMSYNC",
+      home: Scaffold(appBar: AppBar()),
     );
   }
 }
---
+++ App/ktu/lib/domain/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xffFF9418);
+  static const secondaryColor = Color(0xFFFFF8DC);
+  static const lightPrimary = Color(0xFFF1CD91);
+  static const errorColor = Color(0xFFB00020);
+  static const textColor = Color(0xFF000000);
+  static const textSecondary = Color(0xFF848484);
+}
